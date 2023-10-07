178246467112d138bd3eb792bdc6a5a179eee7ef
Tyler Flinchum <tflinchu@emich.edu>
2023-10-07 12:05:12 -0400

    Changing a line of code

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Tjcool313',
+  password: '<insert password here>',
   database: 'epay',
 });
 
