b58ebb53735da9ff3f4d4271d084d7c4993bac67
jacobOats <jacoboats9@gmail.com>
2023-10-08 13:53:42 -0400

    commit for pull

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 const mysql = require('mysql');
- 
-const connection = mysql.createConnection({
+
+const connection = createConnection({
   host: 'localhost',
   user: 'username',
-  password: 'password',
-  database: 'mydb'
+  pass: 'password',
+  database: 'epay'
 });
- 
+
 connection.connect((error) => {
   if (error) throw error;
   console.log('Connected to MySQL database!');
