89a086fb18abd16b816a17ec72f3d0fbc6201c82
TheShakeSnake <145395119+TheShakeSnake@users.noreply.github.com>
2023-11-21 14:09:40 -0500

    added basic post "carousel"

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function loadData(){
         .then(data => {
             var str ="";
             for(var i=0;i<data.length;i++){
-                str += '<tr><td>'+data[i].image+'</td><td><a href="displayitem.html" onclick = "itemClicked(this)">'+data[i].name+'</a></td><td>'+data[i].price+'</td><td>'+data[i].description+'</td></tr>';
+                str += '<li class = "productpost"><div class = "product"><div class = "imagecontainer">' +data[i].image+ '</div><div class = "productdesc"><a href="displayitem.html" onclick = "itemClicked(this)">'+data[i].name+'</a><br>'+data[i].price+'<br>'+data[i].description+'</div></li>';
             }
             document.getElementById("dataTable").innerHTML = str;
         })
@@ -41,7 +41,7 @@ function searchDB(){
         var str = "";
         //fill table with returned rows
         for(var i=0;i<data.length;i++){
-            str += '<tr><td>'+data[i].image+'</td><td><a href="displayitem.html" onclick = "itemClicked(this)">'+data[i].name+'</a></td><td>'+data[i].price+'</td><td>'+data[i].description+'</td></tr>';
+            str += '<li class = "productpost"><div class = "product"><div class = "imagecontainer">' +data[i].image+ '</div><div class = "productdesc"><a href="displayitem.html" onclick = "itemClicked(this)">'+data[i].name+'</a><br>'+data[i].price+'<br>'+data[i].description+'</div></li>';
         }
         document.getElementById("dataTable").innerHTML = ""; //clear previous table
         document.getElementById("dataTable").innerHTML = str;
