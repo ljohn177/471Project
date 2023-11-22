6af000e8f98419196584582737039cd6808a3d55
Tyler Flinchum <tflinchu@emich.edu>
2023-11-22 16:30:00 -0500

    changed the whole product container to be clickable, instead of just the name

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ function loadData(){
         .then(response => response.json())
         .then(data => {
             var str ="";
-            for(var i=0;i<data.length;i++){
-                str += '<li class = "productpost"><div class = "product"><div class = "imagecontainer"><img src=\"' +data[i].image+ '\"></div><div class = "productdesc"><a href="displayitem.html" onclick = "itemClicked(this)">'+data[i].name+'</a><br>'+data[i].price+'<br>'+data[i].description+'</div></li>';
-            }
+for (var i = 0; i < data.length; i++) {
+    str += '<li class="productpost" onclick="itemClicked(this)"><div class="product"><div class="imagecontainer"><img src="' + data[i].image + '"></div><div class="productdesc"><a href="displayitem.html">' + data[i].name + '</a><br>' + data[i].price + '<br>' + data[i].description + '</div></div></li>';
+}
+
             document.getElementById("dataTable").innerHTML = str;
         })
         .catch(error => {
@@ -52,22 +53,25 @@ function searchDB(){
 }
 
 //function to determine which item was clicked and display that item on displayitem.html
-function itemClicked(element){
+function itemClicked(element) {
     //find element in db based on name
-    var name = element.innerHTML;
+    var name = element.querySelector('.productdesc a').innerHTML;
     fetch('/findId', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({name}),
+        body: JSON.stringify({ name }),
     })
-    .then(response => response.json())
-    .then(data => {
-        //store product id in local storage
-        localStorage.setItem("productId", data[0].product_id);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            //store product id in local storage
+            localStorage.setItem("productId", data[0].product_id);
+            //redirect to displayitem.html
+            window.location.href = "displayitem.html";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
+
